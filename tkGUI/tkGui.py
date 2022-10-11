3aee0178d183a2d18107d2909685eda30177e668
ryan_on_windows10 <2922437656@qq.com>
2022-10-11 19:03:27 +0800

    增加了给巡检装置用的UI

--- tkGUI/tkGui.py
+++ tkGUI/tkGui.py
@@ -1,4 +1,5 @@
 # from tkinter import *
+from connection_pool.uart_connection import ble_uart
 import tkinter as tk
 
 
@@ -6,23 +7,35 @@ apple=6.5
 pear=5.4
 bana=7.2
 top=tk.Tk()
+top.geometry("500x500")
 top.title("物联网竞赛")
 labels=[]
 entrys=[]
+def make_show(temperature,humidity,smoke):
+    s=f'''
+    温度: {temperature}
+    湿度: {humidity}
+    烟雾: {smoke}
+    '''
+    return s
 
-label1=tk.Label(top,text="请输入苹果的重量:")
-label2=tk.Label(top,text="请输入梨的重量:")
-label3=tk.Label(top,text="请输入香蕉的重量:")
-entry1=tk.Entry(top)
-entry2=tk.Entry(top)
-entry3=tk.Entry(top)
-# t1=tk.Text(top)
+def refreshText(text:tk.Text,str_show):
+    text.delete(0.0, tk.END)
+    text.insert(tk.INSERT, str_show)
+    text.update()
+
+
+label1=tk.Label(top,text="要发送的控制指令:")
+label3=tk.Label(top,text="当前的信息:")
+entry1=tk.Text(top,width=200,height=20)
+# entry2=tk.Entry(top)
+entry3=tk.Text(top,width=200,height=10)
 labels.append(label1)
-labels.append(label2)
+# labels.append(label2)
 labels.append(label3)
 
 entrys.append(entry1)
-entrys.append(entry2)
+# entrys.append(entry2)
 entrys.append(entry3)
 def load_vertical_lables_entrys(labels:list,entrys:list):
     for i in range(len(labels)):
@@ -34,23 +47,23 @@ load_vertical_lables_entrys(labels,entrys)
 
 # list=tk.Listbox(top)
 def button_clicked():
-    count1 = float(entry1.get())
-    count2 = float(entry2.get())
-    count3 = float(entry3.get())
-    text = "名称   数量   价格"
-    text1 = "苹果  " + str(count1) + "k   " + str(apple * count1)
-    text2 = "梨      " + str(count2) + "k   " + str(pear * count2)
-    text3 = "香蕉  " + str(count3) + "k   " + str(bana * count3)
-    text4 = "总价  " + str(apple * count1 + pear * count2 + bana * count3)
-    list.insert(0, text)
-    list.insert(1, text1)
-    list.insert(2, text2)
-    list.insert(3, text3)
-    list.insert(4, text4)
+    try:
+       s=entry1.get("0.0","end").strip()
+       s=s.replace("\n",'')
+       s=s.replace("\r\n",'')
+       ble_uart.write(bytes(s,encoding='utf-8'))
+       ble_uart.write(bytes('\n',encoding='utf-8'))
+       print( entry1.get("0.0", "end").strip())
+       print(entry1.get("0.0", "end"))
+       refreshText(entry1,'发送成功!')
+    except Exception as e:
+        refreshText(entry1, e)
 
 
 
-# btn=tk.Button(top,text="结算",command=button_clicked)
-# btn.pack()
 
-top.mainloop()
+btn=tk.Button(top,text="发送控制",command=button_clicked)
+btn.pack()
+
+def ui_main():
+    top.mainloop()
