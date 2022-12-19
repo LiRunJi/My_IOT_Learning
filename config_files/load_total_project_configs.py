fa40d2013e5609200fe9df4110859aa289326b22
ryan_on_windows10 <2922437656@qq.com>
2022-12-19 19:11:30 +0800

    修改了区分不同平台的方法

--- config_files/load_total_project_configs.py
+++ config_files/load_total_project_configs.py
@@ -1,27 +1,23 @@
 import yaml
-# import os
-#
-# path = os.getcwd()  # 获取当前路径
-#
-# # print(path)
-#
-# config_file_path = os.path.join(path, "appConfig.yml")
-# print(config_file_path)
 import platform
-my_ubuntu='#54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022'
-my_tencent='#1 SMP Wed May 18 16:02:34 UTC 2022'
+# ubuntu='#54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022'
+# tencent='#1 SMP Wed May 18 16:02:34 UTC 2022'
 with open("config_files/appConfig.yml",'r',encoding='utf-8') as f:
     confs = yaml.load(f.read(), Loader=yaml.FullLoader)
-
-#在这里挑选运行对应的服务器配置:
-if platform.version()==my_ubuntu:
-    conf=confs['on_ubuntu_vm']
-    print("on_ubuntu")
-elif platform.version()==my_tencent:
-    conf=confs['on_win_to_tencent']
-    print("on_tencent")
-else:
-    conf = confs['on_win_to_ubuntu']
+'''
+在腾讯云服务器上和Windows上还有虚拟机上边是三套不同的配置,
+通过platform库进行区分,以加载相应不同的配置
+'''
+if platform.system()=="Windows":
+    conf = confs['on_win_to_tencent']
     print(f"unknown platform: {platform.version()}")
+else:
+    if platform.version().__contains__("Ubuntu"):
+        conf = confs['on_ubuntu_vm']
+        print("on_ubuntu")
+    else:
+        conf = confs['on_win_to_tencent']
+        print("on_tencent")
+
 print()
 print(conf)
