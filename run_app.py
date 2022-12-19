11b38cf2b2ef5068f53975270118bcda249d6fae
ryan_on_windows10 <2922437656@qq.com>
2022-12-19 19:50:18 +0800

    修复了不能执行的bug

--- run_app.py
+++ run_app.py
@@ -6,7 +6,7 @@
 '''
 import os
 import sys
-python="/usr/local/bin/python3.10"
+python="/usr/local/bin/uvicorn"
 host="0.0.0.0"
 if len(sys.argv)<2:
     os.system(f"sudo nohup {python} main:app --host {host} --port 80 &")
