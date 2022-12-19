ee112bb2938d71122f665382e5c5523ebdc1dc72
ryan_on_windows10 <2922437656@qq.com>
2022-12-19 18:46:05 +0800

    增加了对数据的校验,没时间的自动添加时间,没传感器的自动添加传感器

--- DianDongChe/Services/DataAddService.py
+++ DianDongChe/Services/DataAddService.py
@@ -11,6 +11,7 @@ from DianDongChe.schemas import schemas
 #====以上是官方库,下边的是自己写的=======
 from DianDongChe.Dao import SensorsDao
 from DianDongChe.models.Sensor import sensors_data as model_sensor_data
+from datetime import datetime
 router = APIRouter(
     prefix="/DianDongChe_add",
     tags=['拆入数据']
@@ -38,6 +39,11 @@ def add_lots_of_data_by_name(datas_json):
 async def add_one_sensor_data(request:schemas.sensors_datas,db:Session = Depends(get_db)):
     for sensor_data in request.sensors_datas:
         sensor_id = SensorsDao.get_id_by_name(sensor_data.sensor_name, db)
+        if sensor_id is None:
+            sensor_id=SensorsDao.add_one_sensor(sensor_name=sensor_data.sensor_name,db=db)
+        if sensor_data.time is None or sensor_data.time=='' or sensor_data.time=="string":
+            sensor_data.time=datetime.now().__str__().split('.')[0]
+
         SensorsDao.add_one_data(
                                 model_sensor_data(
                                 sensor_id=sensor_id,
